c4ef79d096e21962aa6f9340870aa995bf8b5c00
vandan08 <127378451+vandan08@users.noreply.github.com>
2023-04-26 22:43:53 +0530

    Create bub.cpp

---
+++ bub.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void printarray(int a[],int n)
+{
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<" ";
+	}
+}
+
+void bubblesort(int a[],int n){
+	int temp;
+	for(int i=0;i<n-1;i++){   //for number of passes 
+		for(int j=0;j<n-1-i;j++){
+			if(a[j]>a[j+1]){
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	
+	}
+}
+
+int main(){
+
+	int a[]={32,42,23,45,64,21};
+	int n=6;
+	printarray(a,n);
+	bubblesort(a,n);
+	printarray(a,n);
+
+	return 0;
+}
